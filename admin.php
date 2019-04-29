03b4397dd1cb8ade6cc82dbc6a15f209db94f6de
Alföldi Henrik <alfoldi.henrik@gmail.com>
2019-04-29 08:45:09 +0200

    V2.0

---
+++ admin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+                include('./connect.inc.php');
+                $azon=$_SESSION[id];
+        ?>
+    </head>
+    <body>
+            
+            <header>
+                <h2>Admin</h2>
+            </header>
+        <center>
+            <form action="admin.php" method="POST">
+                <input type="search" name="ker" Placeholder="Keresés">
+                <input type="submit" name="keres" value="Keresés">
+            </form>
+            <table>
+                <?php 
+                    error_reporting( error_reporting() & ~E_NOTICE );
+                    
+                    if(isset($azon)){
+                        if(isset($_POST["keres"])){
+                            $ker=$_POST["ker"];
+                            $sql="select * from main where nam LIKE  '$ker%' order by nam ASC";}
+                        else{$sql="select * from main order by nam ASC";}
+                        $nez=mysqli_query($kapcs,$sql);
+                        while($sor=mysqli_fetch_assoc($nez)){
+                            printf("<tr><td><a>%s</a></td><td><input type='image' src='./pics/user.png' name='x' style='cursor:pointer;' value='%s' alt='Submit' width='24' height='24'></td></tr>",$sor["nam"],$sor["id"]);
+                            if(isset($_POST["x"])){
+                                $_SESSION["user"]=$_POST["x"];
+                                header("location:admkez.php");
+                            }
+                        }
+                    }
+                    else {header("location:kij.php");}
+                ?>
+            </table>
+        </center>
+    </body>
+</html>
--- galery.php
+++ galery.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link rel="shortcut icon" type="image/x-icon" href="letöltés.png" />
-    <title>Helpi</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
-  <meta charset="utf-8">
-
+    <?php
+    include('./connect.inc.php');
+    
+    ?>
   </head>
   <body>
-    <div class="header">
-        <h2>Kép</h2>
-    </div>
-    
+    <header>
+      <h2>
+        <?php
+          if(isset($azon)){
+            $sql="SELECT nam from main where '$azon'=id";
+            $nez=mysqli_query($kapcs,$sql);
+            $sor=mysqli_fetch_assoc($nez);
+            print_r($sor['nam']);
+          }else{echo"Galéria";}
+        ?>
+      </h2>
+    </header>
+    <?php
+      $sql="SELECT pics FROM `pictures` WHERE 1";
+      $nez=mysqli_query($kapcs,$sql);
+      while($sor=mysqli_fetch_assoc($nez)){
+        printf("<img src='%s'>",$sor['pics']);
+      }
+    ?>
   </body>
 </html>
